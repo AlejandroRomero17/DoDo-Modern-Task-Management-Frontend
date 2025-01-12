5d82365cfdeb925b7d931c328fdf5bbef4d9f444
AlejandroRomero17 <gonzalez.romero.alejandroo@gmail.com>
2025-01-11 22:06:51 -0600

    Add login route and controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from "express";
 import User from "../model/user.model";
-// import User from "../model/User.model";
+import jwt from "jsonwebtoken";
+
+require("dotenv").config();
+const secretKey = process.env.JWT_SECRET || '';  // Asegúrate de que JWT_SECRET esté cargado correctamente
 
 // Mensajes de respuesta
 const MESSAGES = {
@@ -8,6 +11,8 @@ const MESSAGES = {
   REGISTRATION_SUCCESS: "User registered successfully",
   SERVER_ERROR: "Internal server error",
   INVALID_DATA: "Invalid user data",
+  LOGIN_FAILED: "Authentication failed",
+  INVALID_PASSWORD: "Wrong password",
 };
 
 // Controlador para registrar un usuario
@@ -34,7 +39,7 @@ const registerUser = async (req: Request, res: Response): Promise<void> => {
 
     res.status(201).json({ message: MESSAGES.REGISTRATION_SUCCESS });
     const result = await user.save();
-    console.log(result)
+    console.log(result);
   } catch (error) {
     console.error("Error registering user:", error);
 
@@ -43,7 +48,46 @@ const registerUser = async (req: Request, res: Response): Promise<void> => {
   }
 };
 
+const loginUser = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { userName, password } = req.body;
+    const user = await User.findOne({ userName });
+    if (!user) {
+      res.status(404).json({ message: MESSAGES.LOGIN_FAILED });
+      return;
+    }
+    const isPasswordValid = await user.comparePassword(password);
+    if (!isPasswordValid) {
+      res.status(404).json({ message: MESSAGES.INVALID_PASSWORD });
+      return;
+    }
+
+    // Asegúrate de que el secretKey esté definido
+    if (!secretKey) {
+      res.status(500).json({ message: "Secret key not defined" });
+      return;
+    }
+
+    // Aquí pasas el secretKey al segundo parámetro
+    let token = jwt.sign({ userId: user._id }, secretKey, { expiresIn: "1h" });
+
+    let finalData = {
+      userId: user._id,
+      userName: user.userName,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      token,
+    };
+
+    res.send(finalData);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: MESSAGES.SERVER_ERROR });
+  }
+};
+
 // Exportar los controladores
 export default {
   registerUser,
+  loginUser,  // Asegúrate de exportar también el loginUser
 };
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import AuthController from "../controllers/auth.controller";
 
-const router: Router = Router();
+const router = Router();
 
 // Ruta para registrar usuarios
 router.post("/register", AuthController.registerUser);
+router.post("/login", AuthController.loginUser);
 
 export default router;
